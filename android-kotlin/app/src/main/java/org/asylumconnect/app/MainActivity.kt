062ff1cb2426e744f0c165c2982e768285c6fa35
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-03-11 19:03:06 -0400

    experimentation

--- android-kotlin/app/src/main/java/org/asylumconnect/app/MainActivity.kt
+++ android-kotlin/app/src/main/java/org/asylumconnect/app/MainActivity.kt
@@ -1,21 +1,30 @@
 package org.asylumconnect.app
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import android.view.LayoutInflater
-import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -48,26 +57,57 @@ class MainActivity : AppCompatActivity() {
             webSettings.userAgentString = System.getProperty("http.agent")
             webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
         }
+
         @SuppressLint("SetJavaScriptEnabled")
         override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
             if (!url.startsWith("app.inreach.org")) {
-                val bottomSheetDialog = BottomSheetDialog(activity)
-                val bottomSheetView = LayoutInflater.from(activity).inflate(R.layout.bottom_sheet_webview, null)
+                class WebDrawer(context: Context) : BottomSheetDialog(context) {
+                    override fun onStart() {
+                        super.onStart()
+                        val bottomSheet =
+                            findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+                        if (bottomSheet != null) {
+                            val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+                            val screenHeight = activity.resources.displayMetrics.heightPixels
+                            bottomSheetBehavior.isFitToContents = false
+                            bottomSheetBehavior.expandedOffset = (screenHeight * 0.05).toInt(
+                            )
+                            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+//                            view?.findViewById<WebView>(R.id.externalWebView)?.visibility =
+//                                View.INVISIBLE
+//                            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility =
+//                                View.VISIBLE
+                        }
+                    }
+                }
+
+
+                val bottomSheetDialog = WebDrawer(activity)
+                val bottomSheetView =
+                    LayoutInflater.from(activity).inflate(R.layout.bottom_sheet_webview, null)
 
                 val externalWebView = bottomSheetView.findViewById<WebView>(R.id.externalWebView)
                 externalWebView?.webViewClient = ExternalWebViewClient()
                 setWebView(externalWebView)
                 externalWebView?.loadUrl(url)
-                val closeButton = bottomSheetView.findViewById<ImageButton>(R.id.closeButton) // Find close button
+                val closeButton =
+                    bottomSheetView.findViewById<ImageButton>(R.id.closeButton) // Find close button
 
                 closeButton.setOnClickListener {
                     bottomSheetDialog.dismiss()
                 }
-                val screenHeight = activity.resources.displayMetrics.heightPixels
+                val openInBrowserButton =
+                    bottomSheetView.findViewById<ImageButton>(R.id.openInBrowser)
+                openInBrowserButton.setOnClickListener {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(it.context, browserIntent, null)
+                }
+//                val screenHeight = activity.resources.displayMetrics.heightPixels
 
-                bottomSheetDialog.behavior.peekHeight = (screenHeight * 0.95).toInt()
+//                bottomSheetDialog.behavior.peekHeight = (screenHeight * 0.95).toInt()
                 bottomSheetDialog.setContentView(bottomSheetView)
                 bottomSheetDialog.show()
+
 
             } else {
                 view?.loadUrl(url)
@@ -76,12 +116,26 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     private class ExternalWebViewClient : WebViewClient() {
-        // Handle any specific behavior for the external WebView here (optional)
+//        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+//            view?.findViewById<WebView>(R.id.externalWebView)?.visibility = View.GONE
+//            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility = View.VISIBLE
+//
+//        }
+
+//        override fun onPageFinished(view: WebView?, url: String?) {
+        //hide loading image
+
+//            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility = View.INVISIBLE
+        //show webview
+//            view?.findViewById<WebView>(R.id.externalWebView)?.visibility = View.VISIBLE
+//            view?.findViewById<LinearLayout>(R.id.buttonBar)?.visibility = View.VISIBLE
+//        }
     }
 
     @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
-     fun setWebView(wv: WebView) {
+    fun setWebView(wv: WebView) {
         val webSettings = wv.getSettings()
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true

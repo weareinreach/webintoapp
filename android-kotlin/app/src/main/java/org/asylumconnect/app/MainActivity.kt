d29d7c04380c3ec5c5f5fe387f1e6e2318c13e80
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-03-12 12:42:40 -0400

    fixed loading animation, web drawer layout

--- android-kotlin/app/src/main/java/org/asylumconnect/app/MainActivity.kt
+++ android-kotlin/app/src/main/java/org/asylumconnect/app/MainActivity.kt
@@ -8,12 +8,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
@@ -21,138 +20,161 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import android.widget.ProgressBar
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        webView = findViewById(R.id.webView)
-        webView.webViewClient = MainWebviewClient(this)
+  private lateinit var webView: WebView
+  lateinit var progressBar: ProgressBar
+  private val mainUrl: Uri = Uri.parse("https://app.inreach.org")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_main)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+    progressBar = findViewById(R.id.progressBar)
+    webView = findViewById(R.id.webView)
+    webView.webViewClient = MainWebviewClient(this)
 
-        setWebView(webView)
-        webView.loadUrl("https://app.inreach.org")
+    setWebView(webView)
+
+    webView.loadUrl(mainUrl.toString())
+
+  }
+
+  inner class MainWebviewClient(val activity: MainActivity) : WebViewClient() {
+    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
+    fun setWebView(wv: WebView) {
+      val webSettings = wv.getSettings()
+      webSettings.javaScriptEnabled = true
+      webSettings.domStorageEnabled = true
+      webSettings.setSupportMultipleWindows(false)
+      webSettings.javaScriptCanOpenWindowsAutomatically = true
+      webSettings.builtInZoomControls = false
+      webSettings.displayZoomControls = false
+      webSettings.loadWithOverviewMode = true
+      webSettings.useWideViewPort = true
+      webSettings.setSupportZoom(true)
+      webSettings.userAgentString = System.getProperty("http.agent")
+      webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
     }
 
-    private class MainWebviewClient(val activity: MainActivity) : WebViewClient() {
-        @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
-        fun setWebView(wv: WebView) {
-            val webSettings = wv.getSettings()
-            webSettings.javaScriptEnabled = true
-            webSettings.domStorageEnabled = true
-            webSettings.setSupportMultipleWindows(false)
-            webSettings.javaScriptCanOpenWindowsAutomatically = true
-            webSettings.builtInZoomControls = false
-            webSettings.displayZoomControls = false
-            webSettings.loadWithOverviewMode = true
-            webSettings.useWideViewPort = true
-            webSettings.setSupportZoom(true)
-            webSettings.userAgentString = System.getProperty("http.agent")
-            webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-        }
-
-        @SuppressLint("SetJavaScriptEnabled")
-        override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
-            if (!url.startsWith("app.inreach.org")) {
-                class WebDrawer(context: Context) : BottomSheetDialog(context) {
-                    override fun onStart() {
-                        super.onStart()
-                        val bottomSheet =
-                            findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-                        if (bottomSheet != null) {
-                            val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-                            val screenHeight = activity.resources.displayMetrics.heightPixels
-                            bottomSheetBehavior.isFitToContents = false
-                            bottomSheetBehavior.expandedOffset = (screenHeight * 0.05).toInt(
-                            )
-                            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    inner class WebDrawer(context: Context) : BottomSheetDialog(context) {
+      override fun onStart() {
+        super.onStart()
+        val bottomSheet =
+          findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        if (bottomSheet != null) {
+          val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+          val screenHeight = activity.resources.displayMetrics.heightPixels
+          bottomSheetBehavior.isFitToContents = false
+          bottomSheetBehavior.expandedOffset = (screenHeight * 0.05).toInt(
+          )
+          bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
 //                            view?.findViewById<WebView>(R.id.externalWebView)?.visibility =
 //                                View.INVISIBLE
 //                            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility =
 //                                View.VISIBLE
-                        }
-                    }
-                }
+        }
+      }
+    }
 
+    @SuppressLint("SetJavaScriptEnabled", "InflateParams")
+    override fun shouldOverrideUrlLoading(
+      view: WebView?,
+      request: WebResourceRequest
+    ): Boolean {
+      val url = request.url.toString()
+      if (!url.startsWith("app.inreach.org")) {
 
-                val bottomSheetDialog = WebDrawer(activity)
-                val bottomSheetView =
-                    LayoutInflater.from(activity).inflate(R.layout.bottom_sheet_webview, null)
+        val bottomSheetDialog = WebDrawer(activity)
+        val bottomSheetView =
+//                    bottomSheetDialog.setContentView(R.layout.bottom_sheet_webview)
+          LayoutInflater.from(activity)
+            .inflate(R.layout.bottom_sheet_webview, null)
 
-                val externalWebView = bottomSheetView.findViewById<WebView>(R.id.externalWebView)
-                externalWebView?.webViewClient = ExternalWebViewClient()
-                setWebView(externalWebView)
-                externalWebView?.loadUrl(url)
-                val closeButton =
-                    bottomSheetView.findViewById<ImageButton>(R.id.closeButton) // Find close button
+        val externalWebView = bottomSheetView.findViewById<WebView>(R.id.externalWebView)
+        externalWebView?.webViewClient = ExternalWebViewClient()
+        setWebView(externalWebView)
+        externalWebView.visibility = View.INVISIBLE
+        externalWebView?.loadUrl(url)
+        val closeButton =
+          bottomSheetView.findViewById<ImageButton>(R.id.closeButton) // Find close button
 
-                closeButton.setOnClickListener {
-                    bottomSheetDialog.dismiss()
-                }
-                val openInBrowserButton =
-                    bottomSheetView.findViewById<ImageButton>(R.id.openInBrowser)
-                openInBrowserButton.setOnClickListener {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    startActivity(it.context, browserIntent, null)
-                }
+        closeButton.setOnClickListener {
+          bottomSheetDialog.dismiss()
+        }
+        val openInBrowserButton =
+          bottomSheetView.findViewById<ImageButton>(R.id.openInBrowser)
+        openInBrowserButton.setOnClickListener {
+          val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+          startActivity(it.context, browserIntent, null)
+        }
 //                val screenHeight = activity.resources.displayMetrics.heightPixels
 
 //                bottomSheetDialog.behavior.peekHeight = (screenHeight * 0.95).toInt()
-                bottomSheetDialog.setContentView(bottomSheetView)
-                bottomSheetDialog.show()
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
+      } else {
+        view?.loadUrl(url)
+      }
+      return true
+    }
 
+    override fun onPageFinished(view: WebView, url: String) {
+      super.onPageFinished(view, url)
 
-            } else {
-                view?.loadUrl(url)
-            }
-            return true
-        }
+      progressBar.visibility = View.GONE
+
+    }
+  }
+
+  inner class ExternalWebViewClient : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+      view.loadUrl(request.url.toString())
+      return false
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+      super.onPageStarted(view, url, favicon)
+      view?.visibility = View.INVISIBLE
+    }
+
+    // ProgressBar will disappear once page is loaded
+    override fun onPageFinished(view: WebView, url: String) {
+
+      super.onPageFinished(view, url)
+      view.visibility = View.VISIBLE
 
     }
 
-    private class ExternalWebViewClient : WebViewClient() {
-//        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-//            view?.findViewById<WebView>(R.id.externalWebView)?.visibility = View.GONE
-//            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility = View.VISIBLE
-//
-//        }
+  }
 
-//        override fun onPageFinished(view: WebView?, url: String?) {
-        //hide loading image
 
-//            view?.findViewById<TextView>(R.id.imageLoading1)?.visibility = View.INVISIBLE
-        //show webview
-//            view?.findViewById<WebView>(R.id.externalWebView)?.visibility = View.VISIBLE
-//            view?.findViewById<LinearLayout>(R.id.buttonBar)?.visibility = View.VISIBLE
-//        }
-    }
-
-    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
-    fun setWebView(wv: WebView) {
-        val webSettings = wv.getSettings()
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-        webSettings.setSupportMultipleWindows(false)
-        webSettings.javaScriptCanOpenWindowsAutomatically = true
-        //        webSettings.setAllowFileAccess(true);
-        webSettings.builtInZoomControls = false
-        webSettings.displayZoomControls = false
-        webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true
-        //        webSettings.setAllowFileAccessFromFileURLs(true);
+  @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
+  fun setWebView(wv: WebView) {
+    val webSettings = wv.getSettings()
+    webSettings.javaScriptEnabled = true
+    webSettings.domStorageEnabled = true
+    webSettings.setSupportMultipleWindows(false)
+    webSettings.javaScriptCanOpenWindowsAutomatically = true
+    //        webSettings.setAllowFileAccess(true);
+    webSettings.builtInZoomControls = false
+    webSettings.displayZoomControls = false
+    webSettings.loadWithOverviewMode = true
+    webSettings.useWideViewPort = true
+    //        webSettings.setAllowFileAccessFromFileURLs(true);
 //        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setSupportZoom(true)
-        //        webSettings.setDatabaseEnabled(true);
-        webSettings.userAgentString = System.getProperty("http.agent")
-        webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-        //wv.addJavascriptInterface(new JavaScriptInterface(MainActivity.this), "Android");
-        //wv.getSettings().setPluginState(WebSettings.PluginState.ON);
-    }
+    webSettings.setSupportZoom(true)
+    //        webSettings.setDatabaseEnabled(true);
+    webSettings.userAgentString = System.getProperty("http.agent")
+    webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+    //wv.addJavascriptInterface(new JavaScriptInterface(MainActivity.this), "Android");
+    //wv.getSettings().setPluginState(WebSettings.PluginState.ON);
+  }
 }
